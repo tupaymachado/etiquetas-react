4f8a4d8b43a1536d7a0a957cc931fa2fef2e90b0
Tupay Machado <tupay.machado@gmail.com>
2023-08-07 18:05:55 -0300

    a

--- src/Functions.jsx
+++ src/Functions.jsx
@@ -8,31 +8,25 @@ export function HandleFileUpload(event) {
         // Parse data
         const bstr = evt.target.result;
         const workbook = XLSX.read(bstr, { type: 'binary' });
-
         // Get first worksheet
         const sheetName = workbook.SheetNames[0];
-
         // Converta a planilha em JSON
         let data = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName], { header: 1 });
-
         // A linha 6 contém os cabeçalhos (índice 5 porque os arrays começam em 0)
         const headers = data[5];
-
         // Remova as linhas antes e incluindo a linha 6
         data = data.slice(6);
-
         const jsonData = data.map(row => {
             // Verifique se o primeiro valor da linha é um número
             if (isNaN(row[1])) {
                 return null;
             }
-
             let obj = {};
             row.forEach((item, index) => {
                 obj[headers[index]] = item;
             });
             return obj;
-        }).filter(item => item); // Filtrar quaisquer itens nulos
+        }).filter(item => item);
         console.log(jsonData);
     };
 
